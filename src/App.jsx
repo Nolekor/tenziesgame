6d6fe7af17da080b11583d8e6d6726b8140d3358
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-18 22:50:00 +0200

    Add game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
+import { useState } from "react";
 import { Die } from "./components/Die";
+import { nanoid } from "nanoid";
+
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  function generateNewDie() {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    };
+  }
+  function allNewDice() {
+    const tenNewDice = [];
+    for (let i = 0; i < 10; i++) {
+      tenNewDice.push(generateNewDie());
+    }
+    return tenNewDice;
+  }
+  const dices = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  function handleRollClick() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+  }
+
   return (
     <main className="h-screen bg-[#0B2434] p-5">
-      <div className="flex h-[400px] max-w-4xl items-center justify-center rounded bg-[#F5F5F5]">
-        <div className="grid grid-cols-5 grid-rows-2 gap-4">
-          <Die value="1" />
-          <Die value="2" />
-          <Die value="3" />
-          <Die value="4" />
-          <Die value="5" />
-          <Die value="6" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-        </div>
+      <div className="flex h-[400px] max-w-4xl flex-col items-center justify-center rounded bg-[#F5F5F5]">
+        <h1 className="m-0 text-5xl">Tenzies</h1>
+        <p className="mt-0 max-w-xs text-center font-normal">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+        <div className="mt-7 grid grid-cols-5 grid-rows-2 gap-4">{dices}</div>
+        <button
+          className="mt-9 h-9 w-24 cursor-pointer rounded bg-[#5035FF] text-white"
+          onClick={handleRollClick}
+        >
+          Roll
+        </button>
       </div>
     </main>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 export const Die = (props) => {
   return (
-    <div className="align flex h-12 w-12 cursor-pointer items-center justify-center rounded bg-white text-3xl font-bold shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+    <div
+      className={`align ${
+        props.isHeld ? "bg-[#59E391]" : "bg-[#ffffff]"
+      } flex h-10 w-10 cursor-pointer items-center justify-center rounded text-3xl font-bold shadow-[0px_4px_4px_rgba(0,0,0,0.25)]`}
+      onClick={props.holdDice}
+    >
       {props.value}
     </div>
   );
